6f453a2076e7ac34380f9269739a904d47301adc
rahuldshetty <35rahuldshetty@gmail.com>
2018-09-16 17:04:52 +0530

    ver1.0

--- app/src/main/java/com/cldnr/rahul/calendero/Settings.java
+++ app/src/main/java/com/cldnr/rahul/calendero/Settings.java
@@ -1,17 +1,27 @@
 package com.cldnr.rahul.calendero;
 
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import static android.content.Context.MODE_PRIVATE;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class Settings extends Fragment {
+
+    Button clearBtn;
+
+    SQLiteDatabase myDatabase;
+
 
     View view;
 
@@ -25,6 +35,21 @@ public class Settings extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_settings, container, false);
+        clearBtn=view.findViewById(R.id.settingsClear);
+
+        myDatabase = this.getActivity().openOrCreateDatabase("dbstorage",MODE_PRIVATE,null);
+        myDatabase.execSQL("CREATE TABLE IF NOT EXISTS programTable(title VARCHAR(15),description VARCHAR(50),time varchar(5),date varchar(10));");
+
+        clearBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                myDatabase.execSQL("Delete from programTable;");
+                Toast.makeText(view.getContext(),"All contents cleared...",Toast.LENGTH_LONG).show();
+
+            }
+        });
+
         return view;
     }
 
